28915e7dde5c5eb1f6f848a2cc78fabe3bc0d89a
Lixiang "Eric" Luo <30944062+l6l@users.noreply.github.com>
2023-11-17 10:53:46 -0500

    Create iterm2.js

---
+++ iterm2.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#c9ab00", "#00c200", "#c7c400", "#0225c7", "#c930c7", "#00c5c7", "#c7c7c7", "#676767", "#ff6d67", "#5ff967", "#fefb67", "#6871ff", "#ff76ff", "#5ffdff", "#fffeff"]);
+t.prefs_.set('foreground-color', "#c7c7c7");
+t.prefs_.set('background-color', "#303030");
+t.prefs_.set('cursor-color', 'rgba(224,217,185,0.5)');
