120117d9b204527234bfbebcfa017dc1b2a6dbb1
Lixiang "Eric" Luo <30944062+l6l@users.noreply.github.com>
2023-11-17 10:15:45 -0500

    Update blink_theme.js

--- blink_theme.js
+++ blink_theme.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#000000", "#d00e18", "#138034", "#ffcb3e", "#006bb3", "#6b2775", "#384564", "#ededed", "#5d504a", "#f07e18", "#b1d130", "#fff120", "#4fc2fd", "#de0071", "#5d504a", "#ffffff"]);
 t.prefs_.set('foreground-color', "#ededed");
-t.prefs_.set('background-color', "#333333");
+t.prefs_.set('background-color', "#2a2a2a");
 t.prefs_.set('cursor-color', 'rgba(224,217,185,0.5)');
