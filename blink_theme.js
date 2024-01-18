0de91aea17b0798fc37087ec8936400d6f6f16bf
Lixiang "Eric" Luo <30944062+l6l@users.noreply.github.com>
2024-01-18 14:23:38 -0500

    Update blink_theme. To be the same as gnome terminal tango dark

--- blink_theme.js
+++ blink_theme.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#d00e18", "#138034", "#ffcb3e", "#006bb3", "#6b2775", "#384564", "#ededed", "#5d504a", "#f07e18", "#b1d130", "#fff120", "#4fc2fd", "#de0071", "#5d504a", "#ffffff"]);
-t.prefs_.set('foreground-color', "#d8d8d8");
-t.prefs_.set('background-color', "#2e2e2e");
+t.prefs_.set('color-palette-overrides',["#2e3436", "#cc0000", "#4e9a06", "#c4a000", "#3465a4", "#75507b", "#06989a", "#d3d7cf", "#555753", "#ef2929", "#8ae234", "#fce94f", "#729fcf", "#ad7fa8", "#34e2e2", "#eeeeec"]);
+t.prefs_.set('foreground-color', "#d3d7cf");
+t.prefs_.set('background-color', "#2e3436");
 t.prefs_.set('cursor-color', 'rgba(224,217,185,0.5)');
